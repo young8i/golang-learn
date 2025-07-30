e5855db98030069c1008e1cd234e9fb671f7877f
fanhaijian <276835906@qq.com>
2025-07-30 22:50:47 +0800

    feat: 最长公共前缀

---
+++ MetaNode/task01/LongestCommonPrefix.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func getCommonPrefix(strs []string) string {
+	res := make([][]rune, maxStringLength(strs))
+	for _, str := range strs {
+		for i, s := range str {
+			res[i] = append(res[i], s)
+		}
+	}
+	fmt.Println(res)
+	returnStr := []rune{}
+	for _, r := range res {
+		if allCharsSame(r) && len(r) == len(strs) {
+			returnStr = append(returnStr, r[0])
+		}
+	}
+	return string(returnStr)
+}
+
+func maxStringLength(arr []string) int {
+	maxLen := 0
+	for _, str := range arr {
+		if len(str) > maxLen {
+			maxLen = len(str)
+		}
+	}
+	return maxLen
+}
+
+func allCharsSame(arr []rune) bool {
+	if len(arr) == 0 {
+		return true
+	}
+	first := arr[0]
+	for _, v := range arr[1:] {
+		if v != first {
+			return false
+		}
+	}
+	return true
+}
+
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ */
+func main() {
+	fmt.Println("result===>", getCommonPrefix([]string{"flower", "flow", "floight"}))
+}
--- MetaNode/task01/ValidParentheses.go
+++ MetaNode/task01/ValidParentheses.go
@@ -7,23 +7,28 @@ func isValid(s string) bool {
 	left := []rune{'{', '[', '('}
 	right := []rune{'}', ']', ')'}
 	// 切片
-	stack := []rune{}
+	stackLeft := []int{}
+	stackRight := []int{}
 	for _, v := range s {
 		for i := 0; i < len(left); i++ {
 			if v == left[i] {
-				stack = append(stack, v)
+				stackLeft = append(stackLeft, i)
 			}
 		}
-		fmt.Println("stack======>", stack)
 		for i := 0; i < len(right); i++ {
 			if v == right[i] {
-				if len(stack) == 0 {
-					return false
-				}
-				if left[i] != stack[len(stack)-1] {
-					return false
-				}
+				stackRight = append(stackRight, i)
 			}
+		}
+	}
+
+	if !(len(stackLeft) == len(stackRight)) {
+		return false
+	}
+
+	for i := 0; i < len(stackLeft); i++ {
+		if !(stackLeft[i] == stackRight[i]) && !(stackLeft[i] == stackRight[len(stackLeft)-i-1]) {
+			return false
 		}
 	}
 	return true
@@ -34,5 +39,5 @@ func isValid(s string) bool {
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效
  */
 func main() {
-	fmt.Println("result===>", isValid("[{}]"))
+	fmt.Println("result===>", isValid("[({})]"))
 }
