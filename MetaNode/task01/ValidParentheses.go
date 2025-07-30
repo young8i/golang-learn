ff881c75e2623ef79f075227520b8e041e30329a
haijianfan <276835906@qq.com>
2025-07-30 18:32:35 +0800

    feat：字符串处理、栈的使用

--- MetaNode/task01/ValidParentheses.go
+++ MetaNode/task01/ValidParentheses.go
@@ -1,9 +1,32 @@
 package main
 
+import "fmt"
+
 func isValid(s string) bool {
 
-	return false
-
+	left := []rune{'{', '[', '('}
+	right := []rune{'}', ']', ')'}
+	// 切片
+	stack := []rune{}
+	for _, v := range s {
+		for i := 0; i < len(left); i++ {
+			if v == left[i] {
+				stack = append(stack, v)
+			}
+		}
+		fmt.Println("stack======>", stack)
+		for i := 0; i < len(right); i++ {
+			if v == right[i] {
+				if len(stack) == 0 {
+					return false
+				}
+				if left[i] != stack[len(stack)-1] {
+					return false
+				}
+			}
+		}
+	}
+	return true
 }
 
 /**
@@ -11,5 +34,5 @@ func isValid(s string) bool {
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效
  */
 func main() {
-
+	fmt.Println("result===>", isValid("[{}]"))
 }
